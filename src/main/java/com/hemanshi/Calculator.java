8f455355a7e52436a1bd5bc17a75bfcc415d2087
Hemanshi Dobaria <hemanshi.dobaria@gmail.com>
2019-10-27 12:11:22 -0700

    Initial Commit

---
+++ src/main/java/com/hemanshi/Calculator.java
@@ -0,0 +1,13 @@
+package com.hemanshi;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
