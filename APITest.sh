3aa738224f5ed7fa70af3aff2bbe77f27fd90123
Hemanshi Dobaria <hemanshi.dobaria@gmail.com>
2019-10-29 21:46:48 -0700

    Adding API script and making jar runnable.

---
+++ APITest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+java -jar target/calculator-1.0-SNAPSHOT.jar add 1 2
+java -jar target/calculator-1.0-SNAPSHOT.jar mul 1 2
+java -jar target/calculator-1.0-SNAPSHOT.jar div 1 2
+java -jar target/calculator-1.0-SNAPSHOT.jar mod 13 2
+java -jar target/calculator-1.0-SNAPSHOT.jar sub 1 2
